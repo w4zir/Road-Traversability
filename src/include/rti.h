4a9b77c2606d34a2bcc6e2056e9e35cf3a90d75f
w4zir <mmk.wazir@gmail.com>
2017-04-14 14:10:33 +0500

    added files

---
+++ src/include/rti.h
@@ -0,0 +1,488 @@
+/*
+ * RTI.h
+ *
+ *  Created on: Feb 16, 2016
+ *      Author: mudassir
+ */
+
+#ifndef RTI_H_
+#define RTI_H_
+
+
+#include <pcl/pcl_base.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/filters/project_inliers.h>
+//#include <pcl/search/search.h>
+//#include <pcl/search/octree.h>
+#include <pcl/search/search.h>
+#include <pcl/search/kdtree.h>
+//#include <pcl/octree/octree.h>
+#include <pcl/octree/octree_search.h>
+//#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <list>
+#include <math.h>
+#include <time.h>
+#include <dem.h>
+#include <iostream>
+#include <fstream>
+
+namespace pcl
+{
+/** \brief
+ * Implement Digital Elevation Map from road point cloud data.
+ */
+template <typename PointT>
+class PCL_EXPORTS RTI : public DEM<PointT>
+{
+public:
+	typedef pcl::octree::OctreePointCloudSearch<PointT> OcTree;
+	typedef typename OcTree::Ptr OcTreePtr;
+	typedef pcl::search::Search <PointT> KdTree;
+	typedef typename KdTree::Ptr KdTreePtr;
+	typedef pcl::PointCloud <PointT> PointCloud;
+	typedef typename PointCloud::Ptr PointCloudPtr;
+
+	using DEM<PointT>::road_min_x_;
+	using DEM<PointT>::road_max_x_;
+	using DEM<PointT>::road_min_y_;
+	using DEM<PointT>::road_max_y_;
+	using DEM<PointT>::search_;
+	using DEM<PointT>::dem_clusters_;
+	using DEM<PointT>::cloud_dem_;
+	using DEM<PointT>::cloud_project_;
+
+	using DEM<PointT>::setRoadPlaneCoefficients;
+	using DEM<PointT>::computeDEM;
+	using DEM<PointT>::getDEMVisibilityCloud;
+	using PCLBase <PointT>::initCompute;
+	using PCLBase <PointT>::deinitCompute;
+	//   using DEM<PointT>::cloud_project_;
+	//   using DEM<PointT>::cloud_project_;
+	//   using DEM<PointT>::input_;
+
+public:
+
+	/** \brief Constructor that sets default values for member variables. */
+	RTI ();
+
+	/** \brief This destructor destroys the cloud, normals and search method used for
+	 * finding KNN. In other words it frees memory.
+	 */
+	virtual
+	~RTI ();
+
+	/** \brief prm and vehicle parameters. */
+	void
+	loadParameters();
+
+	/** \brief Get current vehicle id. */
+	virtual std::string
+	getVehicleId () const;
+
+	/** \brief Set current vehicle id. */
+	virtual void
+	setVehicleId (std::string vehicle_id);
+
+	/** \brief Set goal biad probability. */
+	virtual void
+	setRandomConfigsFlag (bool is_random_configs);
+
+	/** \brief Set goal biad probability. */
+	virtual bool
+	getImportConfigsFlag () const;
+
+	/** \brief Set goal biad probability. */
+	virtual void
+	setImportConfigsFlag (bool is_import_configs);
+
+	/** \brief Set goal biad probability. */
+	virtual bool
+	getRandomConfigsFlag () const;
+
+	/** \brief Set goal biad probability. */
+	virtual void
+	setGoalBiasProbability (float goal_bias);
+
+	/** \brief Set goal biad probability. */
+	virtual float
+	getGoalBiasProbability () const;
+
+	/** \brief Set known obstacles info. */
+	virtual void
+	setObstaclesInfo (Eigen::MatrixXf &obstacles_info);
+
+	/** \brief  Get known obstacles info. */
+	virtual void
+	getObstaclesInfo (Eigen::MatrixXf &obstacles_info);
+
+	/** \brief Set goal biad probability. */
+	virtual int
+	getConfigCounter () const;
+
+	/** \brief Generate default DEM cells under vehicle body at (0,0,0). */
+	std::vector<int>
+	getConfigurationsValidityStatus ();
+
+	/** \brief Generate default DEM cells under vehicle body at (0,0,0). */
+	std::vector<float>
+	getConfigurationsClearance ();
+
+	/** \brief Generate default DEM cells under vehicle body at (0,0,0). */
+	Eigen::MatrixXf
+	getConfigurations ();
+
+	/** \brief Generate default DEM cells under vehicle body at (0,0,0). */
+	virtual void
+	getConfigurationsOnly (Eigen::MatrixXf &vehicle_configs);
+
+	/** \brief Generate default DEM cells under vehicle body at (0,0,0). */
+	virtual void
+	setConfigurations (Eigen::MatrixXf vehicle_configs);
+
+	/** \brief Get ratios of angles of two configs making an edge of prm graph. */
+	virtual void
+	getEdgeAngelRatios (std::vector<float> &angle_ratio);
+
+	/** \brief Returns dimension of RTI in y direction. */
+	void
+	computeRTI ();
+
+	/** \brief Get the DEM clusters. */
+	virtual void
+	getRTIAdjacencyList (std::vector< std::pair<int,int> >& prm_graph);
+
+	/** \brief Cloud for visibility of RTI. */
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr
+	getRTIVisibilityCloud();
+
+protected:
+
+	/** \brief Allows to set search method that will be used for finding KNN.
+	 * \param[in] search search method to use
+	 */
+	void
+	setDEMSearchMethod (const OcTreePtr& tree);
+
+	/** \brief Generate random vehicle configuration. */
+	virtual Eigen::Vector3f
+	generateRandomConfiguration ();
+
+	/** \brief Find nearest config to the random config. */
+	virtual int
+	findQrandNearestNeighbor (Eigen::Vector3f qrand);
+
+	/** \brief Find distance between two configurations. */
+	virtual float
+	config_dist (Eigen::Vector3f qrand, Eigen::Vector4f qnear);
+
+	/** \brief Find new configuration closer to q_rand from q_near. */
+	virtual Eigen::Vector4f
+	findOptimalConfig (Eigen::Vector3f qrand, Eigen::Vector4f qnear);
+
+	/** \brief Find whether the q_new is in goal area. */
+	virtual bool
+	checkGoalReached (Eigen::Vector4f q_new);
+
+	/** \brief Check whether q_new is safe or not. */
+	virtual int
+	checkConfigSafety (Eigen::Vector4f config);
+
+	/** \brief Returns dimension of RTI in y direction. */
+	virtual void
+	addStartConfigurations();
+
+	/** \brief Generate PRM cloud from matrix. Used for search for neighbors configurations. */
+	virtual void
+	getConfigCloud ();
+
+	/** \brief find configs neighbor index. */
+	virtual void
+	findConfigNeighbours ();
+
+	/** \brief find configs neighbor index. */
+	virtual Eigen::MatrixXf
+	getNeighborSubConfigs(Eigen::Vector4f c_config, Eigen::Vector4f n_config, float neighbor_ratio);
+
+	/** \brief Compute circular motion info of a vehicle config. */
+	virtual Eigen::MatrixXf
+	getConfigCircluarCenters (Eigen::Vector4f config);
+
+	/** \brief Returns dimension of RTI in y direction. */
+	virtual void
+	generateRTIGraph();
+
+	/** \brief Returns dimension of RTI in y direction. */
+	virtual void
+	validateConfigurations ();
+
+	/** \brief Check whether two configs are reachable or not. */
+	virtual bool
+	findConfigsConnectivity (Eigen::Vector4f c_config, Eigen::Vector4f n_config, Eigen::MatrixXf config_circles);
+
+	/** \brief Check whether two configs are reachable or not. */
+	virtual float
+	findConfigsConnectivity (Eigen::Vector4f c_config, Eigen::Vector4f n_config);
+
+	/** \brief Generate all vehicle configurations. */
+	virtual void
+	generateConfigurations ();
+
+	/** \brief Returns dimension of PRM in y direction. */
+	virtual void
+	generateQuasiRandomConfigurations();
+
+	/** \brief Returns dimension of PRM in y direction. */
+	virtual void
+	generateRandomConfigurations();
+
+	/** \brief Generate all vehicle configurations. */
+	virtual bool
+	collisionChecker(Eigen::MatrixXf vehicle_state);
+
+	/** \brief Generate all vehicle configurations. */
+	virtual int
+	collisionAndSafetyChecker(Eigen::MatrixXf vehicle_state);
+
+	/** \brief Generate all vehicle configurations. */
+	virtual float
+	computeConfigClearance(Eigen::Vector3f c_config, Eigen::MatrixXf vehicle_state);
+
+	/** \brief Check whether dem clusters is under the body of vehicle or not. */
+	virtual bool
+	checkDemUnderVehicle (int d_idx, Eigen::MatrixXf vehicle_body_state);
+
+	/** \brief Check whether dem clusters is under the body of vehicle or not. */
+	virtual bool
+	checkPointUnderVehicle (Eigen::Vector3f p, Eigen::MatrixXf vehicle_body_state);
+
+	/** \brief Check whether dem clusters is under the body of vehicle or not. */
+	virtual bool
+	checkCircleCollisdeWithRectangle (Eigen::Vector3f circle, Eigen::MatrixXf vehicle_body_state);
+
+	/** \brief Generate default DEM cells under vehicle body at (0,0,0). */
+	virtual int
+	collisionCheckerWithKnownObstacles (Eigen::MatrixXf vehicle_configs);
+
+	/** \brief Check whether dem clusters is under the body of vehicle or not. */
+	virtual bool
+	checkDemInsideTiresBoundingBoxes (int d_idx, Eigen::MatrixXf clearance_bounding_boxes);
+
+	/** \brief Check whether dem clusters is under the body of vehicle or not. */
+	virtual float
+	computePositiveObstacleDistance (int d_idx, Eigen::MatrixXf vehicle_body_state);
+
+	/** \brief Check whether dem clusters is under the body of vehicle or not. */
+	virtual float
+	computeNegativeObstacleDistance (int d_idx, Eigen::MatrixXf vehicle_body_state);
+
+	/** \brief Check dem clusters under tyre for collision with vehicle. */
+	virtual bool
+	tyreClusterCollisionProcessing(int d_idx);
+
+	/** \brief Check dem clusters under body for collision with vehicle. */
+	virtual bool
+	bodyClusterCollisionProcessing(int d_idx);
+
+
+	/** \brief Returns dimension of RTI in y direction. */
+	virtual Eigen::MatrixXf
+	getVehicleState(Eigen::Vector3f v_config);
+
+	/** \brief Returns dimension of RTI in y direction. */
+	virtual Eigen::MatrixXf
+	getVehicleStateWithClearance(Eigen::Vector3f v_config);
+
+	/** \brief Returns dimension of RTI in y direction. */
+	virtual Eigen::MatrixXf
+	getClearanceBoundingBoxes (Eigen::Vector3f v_config);
+
+	virtual bool
+	preProcessingRTI();
+
+	virtual inline	 double
+	pointDistFromPoint (Eigen::Vector3f point_a, Eigen::MatrixXf point_b)
+	{
+		Eigen::Vector3f ab = point_b - point_a;
+		Eigen::Vector3f ab_sqr = ab.cwiseProduct(ab);
+		float ab_dist = std::sqrt(ab_sqr.sum());
+		return ab_dist;
+	}
+
+	virtual inline	 double
+	pointDistFromLine (Eigen::Vector3f point_p, Eigen::MatrixXf point_a, Eigen::MatrixXf point_b)
+	{
+		Eigen::Vector3f ab = point_b - point_a;
+		Eigen::Vector3f ap = point_p - point_a;
+		float ap_proj = ap.dot(ab)/(ab.norm());
+		Eigen::Vector3f ap_sqr = ap.cwiseProduct(ap);
+		float ap_dist = std::sqrt(ap_sqr.sum());
+		float dist = std::sqrt(ap_dist*ap_dist - ap_proj*ap_proj);
+
+		//		Eigen::Vector3f d1=point-point1;
+		//		Eigen::Vector3f d2=point-point2;
+		//		Eigen::Vector3f d3=point2-point1;
+		//		Eigen::Vector3f p = d1.cross(d2);
+		//		return p.norm()/d3.norm();
+		return dist;
+	}
+	//	virtual inline Eigen::MatrixXf
+	//	vehicleBodyLines (Eigen::MatrixXf vehicle_state)
+	//	{
+	//		Eigen::VectorXf x1 = vehicle_state.row(0).leftCols(4);
+	//		Eigen::Vector4f x2;
+	//		x2 << x1.tail(3),x1(0);
+	//		Eigen::VectorXf y1 = vehicle_state.row(1).leftCols(4);
+	//		Eigen::Vector4f y2;
+	//		x2 << y1.tail(3),y1(0);
+	//
+	//		Eigen::Vector4f x_diff = x2-x1;
+	//		Eigen::Vector4f y_diff = y2-y1;
+	//
+	//		Eigen::MatrixXf slopes = vehicle_state.row(1).leftCols(4).rightShift(1)-vehicle_state.row(1).leftCols(4)
+	//	}
+
+
+
+protected:
+
+	const static float PI_ = 3.14159;
+	const static float MAX_PHI_ = 40;
+	const static float MAX_CLEARANCE_ = 5;
+
+	/** \brief Number of cells in x dimension. */
+	int config_x_count_;
+
+	/** \brief Number of configurations in y dimension. */
+	int config_y_count_;
+
+	/** \brief Number of configurations in y dimension. */
+	int config_theta_count_;
+
+	/** \brief Number of configurations attempted before calling it a fail. */
+	int configs_limit_;
+
+	/** \brief Number of start configurations added to the graph. */
+	int start_configs_;
+
+	/** \brief Counter that stores the number of configurations added to the tree. */
+	int config_counter_;
+
+	/** \brief Current vehicle id. */
+	std::string vehicle_id_;
+
+	/** \brief Number of configurations in y dimension. */
+	float min_theta_;
+
+	/** \brief Number of configurations in y dimension. */
+	float max_theta_;
+
+	/** \brief Vehicle velocity per second. */
+	float vehicle_velocity_;
+
+	/** \brief Change in steering angle of the vehicle per second. */
+	float vehicle_steering_rate_;
+
+	/** \brief Number of cells in x dimension. */
+	float vehicle_width_;
+
+	/** \brief Number of cells in z dimension. */
+	float vehicle_length_;
+
+	/** \brief Number of cells in z dimension. */
+	float vehicle_heigt_;
+
+	/** \brief Number of cells in z dimension. */
+	float wheelbase_;
+
+	/** \brief Number of cells in z dimension. */
+	float front_track_;
+
+	/** \brief Number of cells in z dimension. */
+	float ground_clearance_;
+
+	/** \brief Number of cells in z dimension. */
+	float tire_width_;
+
+	/** \brief Number of cells in z dimension. */
+	float negative_obs_threshold_;
+
+	/** \brief Number of cells in z dimension. */
+	int max_config_neighbors_;
+
+	/** \brief Number of cells in z dimension. */
+	int sub_config_count_;
+
+	/** \brief Goal tolerance. */
+	float goal_tolerance_;
+
+	/** \brief Goal biad probability. */
+	float goal_bias_;
+
+	/** \brief Goal tolerance. */
+	float vehicle_safety_dist_;
+
+	/** \brief Radius for config neighbors. */
+	float config_neighbour_radius_;
+
+	/** \brief Step size for sub configs between two connected configs in prm. */
+	float prm_step_size_;
+
+	/** \brief Number of cells in z dimension. */
+	bool is_random_configs_;
+
+	/** \brief Configurations import flag. */
+	bool is_import_configs_;
+
+	/** \brief Flag to show obstacles are known and imported. */
+	bool is_obstacle_known_;
+
+	/** \brief Search method that will be used for KNN. */
+	OcTreePtr dem_search_;
+
+	/** \brief Search method that will be used for neighbor configs. */
+	KdTreePtr config_search_;
+
+	/** \brief Store config cloud for neighbor search. */
+	PointCloudPtr cloud_config_;
+
+	/** \brief Matrix containing all vehicle configurations on road. */
+	Eigen::MatrixXf vehicle_configs_;
+
+	/** \brief Collision status of configurations. */
+	std::vector<int> config_collision_status_;
+
+	/** \brief Vechicle safety values. */
+	std::vector<float> config_clearance_;
+
+	/** \brief PRM edge angle ratio. */
+	std::vector<float> edge_angle_ratio_;
+
+	/** \brief Contains neighbours of each point. */
+	std::vector<std::vector<int> > config_neighbours_;
+
+	/** \brief Matrix containing vehicle state on road. */
+	Eigen::MatrixXf vehicle_state_;
+
+	/** \brief Matrix containing vehicle state on road. */
+	Eigen::MatrixXf obstacles_info_;
+
+	/** \brief Vector containing egdes of RTI graph. */
+	std::vector< std::pair<int,int> > RTI_graph_;
+
+
+public:
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
+///** \brief This function is used as a comparator for sorting. */
+//inline bool
+//comparePair (std::pair<float, int> i, std::pair<float, int> j)
+//{
+//	return (i.first < j.first);
+//}
+}
+//#ifdef PCL_NO_PRECOMPILE
+#include <impl/RTI.hpp>
+//#endif
+#endif /* RTI_H_ */
---
+++ src/road_rti.cpp
@@ -0,0 +1,262 @@
+#include "include/prt.h"
+#include "include/ConfigFile.h"
+
+#include <ctime>
+#include <string>
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/segmentation/sac_segmentation.h>
+
+double get_cpu_time(){
+	return clock() / CLOCKS_PER_SEC;
+}
+template <typename T>
+std::string NumberToString ( T Number )
+{
+	std::ostringstream ss;
+	ss << Number;
+	return ss.str();
+}
+
+int main(int argc, char *argv[])
+{
+	srand(time(0));
+	pcl::PCDReader reader;
+	pcl::PCDWriter writer;
+
+	std::clock_t    start;
+
+	/*
+	 * Read paths from settings.config file and vehicle count from vehicle_parameters.config
+	 */
+	ConfigFile cf("/home/khan/phd_ws/rrlab_code/road_ws/data/settings.config");
+
+	ConfigFile vcf("/home/khan/phd_ws/rrlab_code/road_ws/data/vehicle_parameters.config");
+	int vehicle_count = vcf.Value("vehicle_info","vehicle_count");
+
+	std::cout << vehicle_count << "\t" << vehicle_count << std::endl;
+
+	for (int v_idx = 1; v_idx <= vehicle_count; v_idx++)
+	{
+		std::string vehicle_id = "vehicle" + NumberToString(v_idx);
+		std::string pointcloudFolder = (std::string)cf.Value("paths","pointcloud_folder");
+		std::string configFolder = (std::string)cf.Value("paths","configuations_folder")  + vehicle_id + "/";
+		std::string adjFolder = (std::string)cf.Value("paths","adjaceny_folder")  + vehicle_id + "/";
+		std::string fileStartName = ".pcd";
+
+		std::cout << vehicle_id << "\n" << pointcloudFolder << "\n" << configFolder << "\n" << adjFolder << "\n";
+
+		//    std::string pointcloudFolder = "/home/khan/phd_ws/traversability/pointclouds/rsi/";
+		//    std::string configFolder = "/home/khan/phd_ws/traversability/configs/rsi/rand_01/";
+		//    std::string adjFolder = "/home/khan/phd_ws/traversability/adjacency/rsi/rand_01/";
+		//    std::string fileStartName = ".pcd";
+
+		//	std::string pointcloudFolder = "/home/wazir/phd_ws/traversability/pointclouds/tmp/";
+		//	std::string configFolder = "/home/wazir/phd_ws/traversability/configs/tmp/";
+		//	std::string adjFolder = "/home/wazir/phd_ws/traversability/adjacency/tmp/";
+		//	std::string fileStartName = "clear2.pcd";
+
+		DIR *dir;
+		struct dirent *ent;
+		if ((dir = opendir (pointcloudFolder.c_str())) != NULL) {
+			while ((ent = readdir (dir)) != NULL) {
+				std::string fName = ent->d_name;
+				if(fName.find(fileStartName)!=std::string::npos)
+				{
+					bool fileProcessed = false;
+					DIR *dirPRM;
+					struct dirent *entPRM;
+					if ((dirPRM = opendir (adjFolder.c_str())) != NULL) {
+						while ((entPRM = readdir (dirPRM)) != NULL) {
+							std::string fNamePRM = entPRM->d_name;
+							if(fNamePRM.find("_adj")!=std::string::npos) {
+								std::string fProcessed = fNamePRM.substr(0,fNamePRM.find("_adj"));
+								std::string f2Process = fName.substr(0,fName.find(".pcd"));
+								if (f2Process.compare(fProcessed)==0) {
+									fileProcessed = true;
+									std::cout <<"file processed already"<<f2Process<<"\t"<<fProcessed<<std::endl;
+									break;
+								}
+							}
+						}
+					}
+
+					if(fileProcessed) {
+						continue;
+					}
+
+					//				double cpu1  = get_cpu_time();
+					std::cout << endl<<"reading pointcloud file -------------------------------------\t"<<fName.c_str() << std::endl;
+					std::stringstream readFile;
+					readFile << pointcloudFolder.c_str() <<fName.substr(0,fName.find(".pcd")) << ".pcd";
+					pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZ>);
+					reader.read (readFile.str(), *cloud_in);
+
+					std::cout << "Points in cloud before filtering: " << cloud_in->points.size() << std::endl;
+
+					start = std::clock();
+
+					/*
+					 * reading obstacle file
+					 */
+					Eigen::MatrixXf obstacles_info_tmp = Eigen::MatrixXf::Constant(1000,4,-1000);
+					Eigen::MatrixXf obstacles_info;
+					std::stringstream readObstaclesInfoFile;
+					readObstaclesInfoFile << pointcloudFolder.c_str() << fName.substr(0,fName.find(".pcd")) << "_obs";
+					std::cout <<"file name:\t" << readObstaclesInfoFile.str() << std::endl;
+					std::ifstream obsFile (readObstaclesInfoFile.str().c_str());
+					if (obsFile.is_open())
+					{
+
+						//cout<< "writing data to road_"<<fileIdx<<endl;
+						int c_id = 0;
+						while (!obsFile.eof())
+						{
+							std::string line;
+							std::getline (obsFile,line);
+							if(line.size()==0)
+							{
+								break;
+							}
+							std::size_t nospace1 = line.find_first_not_of(" ");
+							std::size_t space1 = line.find(" ",nospace1);
+							std::size_t nospace2 = line.find_first_not_of(" ",space1);
+							std::size_t space2 = line.find(" ",nospace2);
+							std::size_t nospace3 = line.find_first_not_of(" ",space2);
+							std::size_t space3 = line.find(" ",nospace3);
+							std::size_t nospace4 = line.find_first_not_of(" ",space3);
+							std::size_t space4 = line.find(" ",nospace4);
+
+							std::string xStr = line.substr(nospace1,space1-1);
+							std::string yStr = line.substr(nospace2,space2-1);
+							std::string rStr = line.substr(nospace3,space3-1);
+							std::string sStr = line.substr(nospace4,space4-1);
+							float xVal = std::atof(xStr.c_str());
+							float yVal = std::atof(yStr.c_str());
+							float rVal = std::atof(rStr.c_str());
+							float sVal = std::atof(sStr.c_str());
+							// std::cout <<c_id << ":\t line:	" << line <<"\t," << space1 <<"," << space2 <<"," << space3 << std::endl;
+
+							obstacles_info_tmp.row(c_id) = Eigen::Vector4f(xVal,yVal,rVal,sVal);
+							c_id++;
+							//std::cout <<c_id << ":\t" << xVal <<"," << yVal <<"," << rVal <<"," << sVal << std::endl;
+
+						}
+						obsFile.close();
+						obstacles_info = obstacles_info_tmp.topRows(c_id);
+					}
+					std::cout << obstacles_info.rows() <<" obstacles found.\n" << obstacles_info << std::endl;
+
+					// Create a set of planar coefficients with X=Y=0,Z=1
+					pcl::ModelCoefficients::Ptr proj_plane_coefficients (new pcl::ModelCoefficients ());
+					proj_plane_coefficients->values.resize (4);
+					proj_plane_coefficients->values[0] = proj_plane_coefficients->values[1] = 0;
+					proj_plane_coefficients->values[2] = 1.0;
+					proj_plane_coefficients->values[3] = 0;
+
+					pcl::PRT<pcl::PointXYZ> prtObj;
+					prtObj.setVehicleId(vehicle_id);
+					prtObj.setInputCloud(cloud_in);
+					prtObj.SetProjectedPlaneCoefficients(proj_plane_coefficients);
+					prtObj.setRandomConfigsFlag(true);
+					prtObj.setObstaclesInfo(obstacles_info);
+					prtObj.computePRT();
+					std::vector< std::pair<int,int> > prt_graph;
+					prtObj.getPRTAdjacencyList(prt_graph);
+					std::vector<float> prm_edge_angel_ratios;
+					prtObj.getEdgeAngelRatios(prm_edge_angel_ratios);
+
+					std::cout << "Time: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
+
+					/*
+					 * save adjacency
+					 */
+					std::stringstream saveAdj;
+					saveAdj << adjFolder.c_str() << fName.substr(0,fName.find(".pcd")) <<"_adj";
+					std::ofstream saveAdjFile(saveAdj.str().c_str());
+					if (saveAdjFile.is_open())
+					{
+						for (int i=0; i < prt_graph.size(); i++)
+						{
+							saveAdjFile << prt_graph[i].first <<" "<< prt_graph[i].second <<" "<< 1 << " " << prm_edge_angel_ratios[i] << "\n";
+						}
+						saveAdjFile.close();
+					}else {
+						std::cout<<"Error: can not find directory"<<std::endl;
+						exit(0);
+					}
+
+					/*
+					 * save configurations
+					 */
+					Eigen::MatrixXf vehicle_configs = prtObj.getConfigurations();
+					//				int config_counter = PRTObj.getConfigCounter();
+					//				std::cout << "config counter:" << config_counter << std::endl;
+					std::vector<int> config_validity_status = prtObj.getConfigurationsValidityStatus();
+					std::vector<float> config_clearance = prtObj.getConfigurationsClearance();
+
+					std::stringstream savePRT;
+					savePRT << configFolder.c_str() << fName.substr(0,fName.find(".pcd")) <<"_conf";
+					std::ofstream savePRTFile(savePRT.str().c_str());
+					if (savePRTFile.is_open())
+					{
+						for (int i=0; i < vehicle_configs.rows(); i++)
+						{
+							savePRTFile << vehicle_configs.row(i) <<" "<<config_validity_status[i]<< " " << config_clearance[i] << "\n";
+						}
+						savePRTFile.close();
+					}else {
+						std::cout<<"Error: can not find directory"<<std::endl;
+						exit(0);
+					}
+					//				double cpu2  = get_cpu_time();
+					//				std::cout << "CPU Time after prm complete = " << cpu2  - cpu1  << std::endl;
+					std::cout << "file written successfully" << endl<< std::endl;
+
+					//  pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = prtObj.getDEMVisibilityCloud ();
+					//  writer.write<pcl::PointXYZRGB> ("road_rgb.pcd", *colored_cloud, false);
+					//				boost::shared_ptr<pcl::PolygonMesh> mesh_ptr_;
+					//				mesh_ptr_->polygons[0].vertices[0] = 0;
+					//				mesh_ptr_->polygons[0].vertices[1] = 0;
+					//				mesh_ptr_->polygons[0].vertices[2] = 0;
+					//				mesh_ptr_->polygons[1].vertices[0] = 1;
+					//				mesh_ptr_->polygons[1].vertices[1] = 0;
+					//				mesh_ptr_->polygons[1].vertices[2] = 0;
+					//				mesh_ptr_->polygons[2].vertices[0] = 1;
+					//				mesh_ptr_->polygons[2].vertices[1] = 2;
+					//				mesh_ptr_->polygons[2].vertices[2] = 0;
+
+					//				boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+					//				viewer->setBackgroundColor (0, 0, 0);
+					//				pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(colored_cloud);
+					//				viewer->addPointCloud<pcl::PointXYZRGB> (colored_cloud, rgb, "sample cloud");
+					//				viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud");
+					//				viewer->addCoordinateSystem (1.0);
+					//				viewer->initCameraParameters ();
+
+					//				pcl::visualization::CloudViewer viewer ("Cluster viewer");
+
+					//				viewer.addPolygonMesh	(mesh_ptr_,"polygon",0 );
+					//				viewer.showCloud(colored_cloud);
+					//				while (!viewer.wasStopped ())
+					//				{
+					//				}
+
+					//				while (!viewer->wasStopped ())
+					//				{
+					//					viewer->spinOnce (100);
+					//					boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+					//				}
+				}
+			}
+			closedir (dir);
+		} else {
+			perror ("could not open directory");
+			return EXIT_FAILURE;
+		}
+	}
+	return 0;
+}
